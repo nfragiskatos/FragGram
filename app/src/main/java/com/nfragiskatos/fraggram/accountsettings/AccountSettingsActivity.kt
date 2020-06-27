599a25bf109c836cbae7656ac6e9986d55f8c607
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-06-27 11:05:43 -0700

    - Created another activity for Account Settings - Implemented initial Account Settings resource layout.

---
+++ app/src/main/java/com/nfragiskatos/fraggram/accountsettings/AccountSettingsActivity.kt
@@ -0,0 +1,12 @@
+package com.nfragiskatos.fraggram.accountsettings
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.nfragiskatos.fraggram.R
+
+class AccountSettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account_settings)
+    }
+}
