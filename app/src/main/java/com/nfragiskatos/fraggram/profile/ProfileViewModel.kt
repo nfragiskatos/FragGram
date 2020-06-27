d79234fed27abab328d9edbed0db9b398d56642c
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-06-27 07:41:19 -0700

    - started implementing ViewModel for Profile fragment   - Used the observable + function pattern to kick off a navigation to the edit profile screen

--- app/src/main/java/com/nfragiskatos/fraggram/profile/ProfileViewModel.kt
+++ app/src/main/java/com/nfragiskatos/fraggram/profile/ProfileViewModel.kt
@@ -1,7 +1,20 @@
 package com.nfragiskatos.fraggram.profile
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ProfileViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val _navigateToEditProfileActivity = MutableLiveData<Boolean>()
+    val navigateToEditProfileActivity: LiveData<Boolean>
+        get() = _navigateToEditProfileActivity
+
+    fun displayEditProfileActivity() {
+        _navigateToEditProfileActivity.value = true
+    }
+
+    fun displayEditProfileActivityComplete() {
+        _navigateToEditProfileActivity.value = false
+    }
 }
