10b0d4679bd49db179a1452dac1cbd311c3be189
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-06-27 20:52:12 -0700

    - Gonna refactor this project into a 1-activity app. Changing account settings page from activity to a fragment.

---
+++ app/src/main/java/com/nfragiskatos/fraggram/activities/main/fragments/accountsettings/AccountSettingsViewModel.kt
@@ -0,0 +1,7 @@
+package com.nfragiskatos.fraggram.activities.main.fragments.accountsettings
+
+import androidx.lifecycle.ViewModel
+
+class AccountSettingsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
