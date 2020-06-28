e8f13b7d91079a96a817900399957bcf3e4bf5b2
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-06-28 08:10:14 -0700

    - Created Fragment + ViewModel + layout (with initial design) for the sign in screen.

---
+++ app/src/main/java/com/nfragiskatos/fraggram/activities/main/fragments/signin/SignInViewModel.kt
@@ -0,0 +1,7 @@
+package com.nfragiskatos.fraggram.activities.main.fragments.signin
+
+import androidx.lifecycle.ViewModel
+
+class SignInViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
