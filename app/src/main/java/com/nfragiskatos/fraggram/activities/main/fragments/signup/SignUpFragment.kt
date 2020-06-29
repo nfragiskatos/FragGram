db4d4dee3be6ea94506b42629e629041ca5ab51e
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-06-28 17:55:49 -0700

    - Added notification and log message observers from the view model. - Added a status observer from view model that will display a progress bar when necessary

--- app/src/main/java/com/nfragiskatos/fraggram/activities/main/fragments/signup/SignUpFragment.kt
+++ app/src/main/java/com/nfragiskatos/fraggram/activities/main/fragments/signup/SignUpFragment.kt
@@ -1,9 +1,12 @@
 package com.nfragiskatos.fraggram.activities.main.fragments.signup
 
+import android.app.ProgressDialog
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -12,6 +15,7 @@ import com.nfragiskatos.fraggram.databinding.FragmentSignUpBinding
 
 class SignUpFragment : Fragment() {
 
+    private val TAG = "RegisterFragment"
     companion object {
         fun newInstance() = SignUpFragment()
     }
@@ -20,12 +24,13 @@ class SignUpFragment : Fragment() {
         ViewModelProvider(this).get(SignUpViewModel::class.java)
     }
 
+    private lateinit var binding: FragmentSignUpBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        val binding = FragmentSignUpBinding.inflate(inflater)
+        binding = FragmentSignUpBinding.inflate(inflater)
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
 
@@ -36,6 +41,39 @@ class SignUpFragment : Fragment() {
             }
         })
 
+        viewModel.notification.observe(viewLifecycleOwner, Observer {
+            Toast.makeText(context, it, Toast.LENGTH_SHORT)
+                .show()
+        })
+
+        viewModel.logMessage.observe(viewLifecycleOwner, Observer {
+            Log.d(TAG, it)
+        })
+
+        viewModel.status.observe(viewLifecycleOwner, Observer {status ->
+
+
+            when (status) {
+                SignUpStatus.LOADING -> {
+                    binding.progressBarHolder.visibility = View.VISIBLE
+                    setBackgroundEnabled(false)
+                }
+                else -> {
+                    binding.progressBarHolder.visibility = View.INVISIBLE
+                    setBackgroundEnabled(true)
+                }
+            }
+        })
+
         return binding.root
+    }
+
+    private fun setBackgroundEnabled(isEnabled: Boolean) {
+        binding.edittextFullNameSignUp.isEnabled = isEnabled
+        binding.edittextUsernameSignUp.isEnabled = isEnabled
+        binding.edittextEmailSignUp.isEnabled = isEnabled
+        binding.edittextPasswordSignUp.isEnabled = isEnabled
+        binding.buttonSignUpSignUp.isEnabled = isEnabled
+        binding.buttonSignInSignUp.isEnabled = isEnabled
     }
 }
