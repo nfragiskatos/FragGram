66ecdfce33075e58f4fefb11d04401776bda9554
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-07-03 08:50:15 -0700

    - Added function to sign out when sign out button is clicked. - Added navigation pattern observable + functions to navigate from the account settings screen to the sign in screen.

--- app/src/main/java/com/nfragiskatos/fraggram/activities/main/fragments/accountsettings/AccountSettingsViewModel.kt
+++ app/src/main/java/com/nfragiskatos/fraggram/activities/main/fragments/accountsettings/AccountSettingsViewModel.kt
@@ -1,7 +1,26 @@
 package com.nfragiskatos.fraggram.activities.main.fragments.accountsettings
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class AccountSettingsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val _navigateToSingInFragment = MutableLiveData<Boolean>()
+    val navigateToSingInFragment: MutableLiveData<Boolean>
+        get() = _navigateToSingInFragment
+
+    fun displaySignInFragment() {
+        _navigateToSingInFragment.value = true;
+    }
+
+    fun displaySignInFragmentComplete() {
+        _navigateToSingInFragment.value = false;
+    }
+
+    fun signOutUser() {
+        Firebase.auth.signOut()
+        displaySignInFragment()
+    }
 }
