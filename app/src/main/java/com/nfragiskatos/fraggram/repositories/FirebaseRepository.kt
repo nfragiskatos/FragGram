d23511e0f779f7729f46e81a9102873e1519619d
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-07-03 07:07:25 -0700

    - Added repository method for saving a User to the Firebase Database

--- app/src/main/java/com/nfragiskatos/fraggram/repositories/FirebaseRepository.kt
+++ app/src/main/java/com/nfragiskatos/fraggram/repositories/FirebaseRepository.kt
@@ -1,17 +1,32 @@
 package com.nfragiskatos.fraggram.repositories
 
+import android.util.Log
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.nfragiskatos.fraggram.activities.main.domain.User
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 
 object FirebaseRepository {
 
-    suspend fun performSignUp(email: String, password: String) : AuthResult? {
+    suspend fun performSignUp(email: String, password: String): AuthResult? {
         return withContext(Dispatchers.IO) {
             return@withContext Firebase.auth.createUserWithEmailAndPassword(email, password).await()
         }
+    }
+
+    suspend fun saveUserInfo(user: User, nodePath: String) {
+        withContext(Dispatchers.IO) {
+            val path = validatePath(nodePath)
+            val ref = Firebase.database.getReference(path + user.uid)
+            ref.setValue(user).await()
+        }
+    }
+
+    private fun validatePath(nodePath: String): String {
+        return if (nodePath[nodePath.lastIndex] == '/') nodePath else "$nodePath/"
     }
 }
