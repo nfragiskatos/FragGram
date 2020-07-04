f75ed9b138c3262fc235b723e204c8534a053f1a
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-07-03 17:33:10 -0700

    - Added observables for list of User models, and created a function to set mock data

--- app/src/main/java/com/nfragiskatos/fraggram/activities/main/fragments/search/SearchViewModel.kt
+++ app/src/main/java/com/nfragiskatos/fraggram/activities/main/fragments/search/SearchViewModel.kt
@@ -1,7 +1,23 @@
 package com.nfragiskatos.fraggram.activities.main.fragments.search
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.nfragiskatos.fraggram.activities.main.domain.User
 
 class SearchViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _users = MutableLiveData<MutableList<User>>(mutableListOf())
+    val users: LiveData<MutableList<User>>
+        get() = _users
+
+    // TODO: DELETE
+    fun initMockData() {
+        var data = mutableListOf<User>()
+        data.add(User("", "Harry Potter", "hpotter", "", "", ""))
+        data.add(User("", "Ron Weasley", "rweasley", "", "", ""))
+        data.add(User("", "Hermione Granger", "hgranger", "", "", ""))
+        data.add(User("", "Draco Malfoy", "dmalfoy", "", "", ""))
+        data.add(User("", "Rubeus Hagrid", "rhagrid", "", "", ""))
+        _users.value = data
+    }
 }
