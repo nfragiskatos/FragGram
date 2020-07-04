e5456e3a0f67c4c161687b996b8282f317a199b6
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-07-03 17:28:07 -0700

    - Initial implementation of a RecyclerView for displaying user information when searching. - The layout for the ViewHolder is a profile picture with username, full name, and a button for following the user. - The adapter takes as an argument a click listener for action to be taken when the "follow" button is clicked. - Adapter works off of User domain models.

---
+++ app/src/main/java/com/nfragiskatos/fraggram/activities/main/fragments/search/SearchListAdapter.kt
@@ -0,0 +1,59 @@
+package com.nfragiskatos.fraggram.activities.main.fragments.search
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.nfragiskatos.fraggram.activities.main.domain.User
+import com.nfragiskatos.fraggram.databinding.ListViewUserItemBinding
+
+class SearchListAdapter(private val onFollowClickListener: OnFollowClickListener) :
+    ListAdapter<User, SearchListAdapter.SearchItemViewHolder>(DiffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchItemViewHolder {
+        return SearchItemViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: SearchItemViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item, onFollowClickListener)
+    }
+
+
+    class SearchItemViewHolder private constructor(private val binding: ListViewUserItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(user: User, onFollowClickListener: OnFollowClickListener) {
+            binding.textviewUsernameUserItem.text = user.username
+            binding.textviewFullNameUserItem.text = user.fullName
+            binding.buttonFollowUserItem.setOnClickListener {
+                onFollowClickListener.onClick(user)
+            }
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): SearchItemViewHolder {
+                val binding = ListViewUserItemBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+                return SearchItemViewHolder(binding)
+            }
+        }
+    }
+
+    class OnFollowClickListener(val clickListener: (user: User) -> Unit) {
+        fun onClick(user: User) = clickListener(user)
+    }
+
+    companion object DiffCallback : DiffUtil.ItemCallback<User>() {
+        override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
+            return oldItem === newItem
+        }
+
+        override fun areContentsTheSame(oldItem: User, newItem: User): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
