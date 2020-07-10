bb389d47f1da99f13dc7ec6cb61cc34fc5534854
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-07-09 20:04:31 -0700

    - Initial implementation of Fragment, ViewModel, and layout for add post screen.   - Setup ViewModel and Binding in Fragment   - Setup onClick for the image view to select a photo to upload from phone storage   - Added data binding between ImageView in layout and the Livedata in ViewModel holding the URI for the selected image.

--- app/src/main/java/com/nfragiskatos/fraggram/activities/main/fragments/addpost/AddPostFragment.kt
+++ app/src/main/java/com/nfragiskatos/fraggram/activities/main/fragments/addpost/AddPostFragment.kt
@@ -1,32 +1,55 @@
 package com.nfragiskatos.fraggram.activities.main.fragments.addpost
 
-import androidx.lifecycle.ViewModelProviders
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.nfragiskatos.fraggram.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.nfragiskatos.fraggram.databinding.FragmentAddPostBinding
+import com.theartofdev.edmodo.cropper.CropImage
 
 class AddPostFragment : Fragment() {
+
+    private val TAG = "AddPostFragment"
 
     companion object {
         fun newInstance() = AddPostFragment()
     }
 
-    private lateinit var viewModel: AddPostViewModel
+    private val viewModel: AddPostViewModel by lazy {
+        ViewModelProvider(this).get(AddPostViewModel::class.java)
+    }
+
+    private lateinit var binding: FragmentAddPostBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_add_post, container, false)
+
+        binding = FragmentAddPostBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        binding.imageviewImagePostAddPost.setOnClickListener {
+            context?.let {
+                CropImage.activity().setAspectRatio(2, 1)
+                    .start(it, this)
+            }
+        }
+
+
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(AddPostViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
 
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null) {
+            viewModel.updateProfileImageUri(CropImage.getActivityResult(data).uri.toString())
+        }
+    }
 }
--- app/src/main/java/com/nfragiskatos/fraggram/activities/main/fragments/addpost/AddPostViewModel.kt
+++ app/src/main/java/com/nfragiskatos/fraggram/activities/main/fragments/addpost/AddPostViewModel.kt
@@ -1,7 +1,17 @@
 package com.nfragiskatos.fraggram.activities.main.fragments.addpost
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class AddPostViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _photoUri = MutableLiveData<String>()
+    val photoUri: LiveData<String>
+        get() = _photoUri
+    private var photoChanged: Boolean = false
+
+    fun updateProfileImageUri(uri: String) {
+        _photoUri.value = uri
+        photoChanged = true
+    }
 }
