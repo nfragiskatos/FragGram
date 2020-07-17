6951d2963ff7a3e85681e1bbe8fe1272cdefa0ca
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-07-16 20:17:12 -0700

    - Set the RecyclerView's adapter to the new HomeListAdapter via binding.

--- app/src/main/java/com/nfragiskatos/fraggram/activities/main/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/nfragiskatos/fraggram/activities/main/fragments/home/HomeFragment.kt
@@ -1,13 +1,11 @@
 package com.nfragiskatos.fraggram.activities.main.fragments.home
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.nfragiskatos.fraggram.R
 import com.nfragiskatos.fraggram.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -30,6 +28,7 @@ class HomeFragment : Fragment() {
         binding = FragmentHomeBinding.inflate(inflater)
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
+        binding.recyclerViewActivityHome.adapter = HomeListAdapter()
 
         return binding.root
     }
